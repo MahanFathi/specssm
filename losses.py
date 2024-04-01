4ba531c976160ad4fb4b1246e1458a42af879261
Mahan Fathi <mahan0fathi@gmail.com>
2024-04-01 18:05:39 -0400

    Fix loss to be mean over batch and not sum

--- losses.py
+++ losses.py
@@ -11,7 +11,7 @@ def create_classification_loss():
     def classification_loss_fn(model_outputs, targets):
         num_classes = model_outputs.shape[-1]   # might have to fix later
         one_hot_targets = jax.nn.one_hot(targets, num_classes)
-        loss = jnp.sum(optax.softmax_cross_entropy(model_outputs, one_hot_targets))
+        loss = jnp.mean(optax.softmax_cross_entropy(model_outputs, one_hot_targets))
         metrics = {
             'loss': loss,
             'accuracy': jnp.mean(jnp.argmax(model_outputs, axis=-1) == targets)
