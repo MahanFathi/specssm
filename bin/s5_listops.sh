87597a7aea42ac1d993fe39cfc80dcafe1583343
Mahan Fathi <mahan0fathi@gmail.com>
2024-04-05 14:13:03 -0400

    Update configs and bins

--- bin/s5_listops.sh
+++ bin/s5_listops.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python main.py --gin_file=base.gin --gin_file=task/listops.gin --gin_file=ssm/s5.gin
+python main.py --gin_file=base.gin --gin_file=ssm/s5.gin --gin_file=task/listops.gin
