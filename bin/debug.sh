bb4d7a9c86f41ff91697d920707c7b6bc751e59b
MahanFathi <mahan0fathi@gmail.com>
2024-04-08 02:05:02 -0400

    Debug bin no wandb

--- bin/debug.sh
+++ bin/debug.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python main.py --gin_file=base.gin --gin_file=task/listops.gin --gin_file=ssm/s5.gin --gin_file=size/tiny.gin
+python main.py --gin_file=base.gin --gin_file=task/listops.gin --gin_file=ssm/s5.gin --gin_file=size/tiny.gin --gin_param=train.Trainer.use_wandb=False
